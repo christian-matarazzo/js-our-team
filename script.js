20d4cf204bf8f7a4913cb9bff47f4aca08a88617
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-18 16:24:25 +0100

    add form and submit for other user

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ for (let i = 0; i < teamMembers.length; i++) {
   //we save the variables
   const {name, role, email, img} = thisMember;
 
-   
+   //we manipulate the HTML with our markup 
  cards += `
     <div class="col-12 col-md-6 col-lg-4 mb-4">
         <div class="card"> 
@@ -78,4 +78,40 @@ for (let i = 0; i < teamMembers.length; i++) {
     `;
 }
 
-cardContainerEl.innerHTML = cards
+cardContainerEl.innerHTML = cards //we call the markup form JS to HTML
+
+
+//add a form check to get the submit from user
+const formEl = document.getElementById("form-submit");
+
+formEl.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value; //get the values from form sub
+    const role = document.getElementById("role").value; 
+    const email = document.getElementById("email").value;
+
+    const newCardHTML = `
+    <div class="col-12 col-md-6 col-lg-4 mb-4">
+        <div class="card h-100 border-0 shadow-sm overflow-hidden">
+            <div class="row g-0 h-100">
+                <div class="col-4">
+                    <img src="./img/bigboss.webp" class="img-fluid h-100" style="object-fit: cover;" alt="${name}">
+                </div>
+                <div class="col-8">
+                    <div class="card-body bg-dark text-white h-100"> 
+                        <h5 class="card-title mb-1">${name}</h5>
+                        <p class="card-text mb-1">${role}</p>
+                        <p class="card-text"><small class="text-info">${email}</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+ 
+    cardContainerEl.innerHTML += newCardHTML; //we add the new card into the HTML
+
+formEl.reset();
+});
